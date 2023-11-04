ac71560cf40534a0a016dacb2ffc05ef91b48f5d
Rohit Kumar <145838244+rohitwithcode@users.noreply.github.com>
2023-11-04 20:33:46 +0530

    insert single row query added

---
+++ insert_single_row_query.sql
@@ -0,0 +1,5 @@
+-- inert single row query
+insert into users
+(id,name,email,pass,contact,address,dob,gender,status) 
+values 
+(1,"Rohit","rrr@gmail.com","1234","94949494","kunjalpur","2004-02-12","Male",1)
