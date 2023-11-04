a59b7cdc5d65f1d9485a33f22ec37478d3afe1ea
Rohit Kumar <145838244+rohitwithcode@users.noreply.github.com>
2023-11-04 20:34:16 +0530

    insert multiple row query added

---
+++ insert_multiple_row_query.sql
@@ -0,0 +1,8 @@
+-- inert single row query
+insert into users
+(id,name,email,pass,contact,address,dob,gender,status) 
+values 
+(1,"Rohit","rrr@gmail.com","1234","94949494","kunjalpur","2004-02-12","Male",1),
+(2,"Rohit Kumar","rrr@gmail.com","1234","94949494","kunjalpur","2004-02-12","Male",1),
+(3,"Mohit","rrr@gmail.com","1234","94949494","kunjalpur","2004-02-12","Male",1),
+(4,"Mohit Yadav","rrr@gmail.com","1234","94949494","kunjalpur","2004-02-12","Male",1)
