68bcfbb7088178b51eeee22590885330e671a875
Rohit Kumar <145838244+rohitwithcode@users.noreply.github.com>
2023-11-08 19:39:31 +0530

    offset query added

---
+++ Offset query.sql
@@ -0,0 +1,8 @@
+/*	
+	You can also specify an offeset from where to start returning data .
+    
+    SYSNTAX :
+    
+    select * from <table name> limit 5 offset [number of rows to skip];
+*/
+select * from users limit 5 offset 4
