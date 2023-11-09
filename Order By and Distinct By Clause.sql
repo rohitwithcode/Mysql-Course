0323511a6d1cd11275db65d7b8c7389277262102
Rohit Kumar <145838244+rohitwithcode@users.noreply.github.com>
2023-11-09 10:07:22 +0530

    order by and distinct by clause

---
+++ Order By and Distinct By Clause.sql
@@ -0,0 +1,5 @@
+-- select * from users order by name asc
+-- 	select * from users order by name desc
+
+--  The function of DISTINCT clause is to bring unique data in the data.
+	select distinct name from users  --  its use for sort data with unique value
